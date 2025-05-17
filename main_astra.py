e156cd71879fe34cda544cc60a1388c94489e452
Priyanjali Goel <p.goel.2@umail.leidenuniv.nl>
2025-05-17 21:50:18 +0200

    add sirt

--- main_astra.py
+++ main_astra.py
@@ -1,25 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from phantoms import generate_phantom
+
 from phantoms import create_phantom # LABEdits
-from utils_astra import generate_sinogram_astra, create_astra_geometry, display_sinogram, add_noise, compute_rmse, compute_rnmp, save_image, reconstruct_fbp_astra
+from utils_astra import generate_sinogram_astra, create_astra_geometry, display_sinogram, add_noise, compute_rmse, compute_rnmp, save_image, reconstruct_fbp_astra, reconstruct_sirt_astra
 from pdmdart_astra import PDMDARTAstra
 
 def main():
     # 1. Generate the phantom
     print("Generating phantom...")
-    phantom = generate_phantom(phantom_type="basic", resolution=256, noise_type=None)
-    phantom = create_phantom(phantom_type='basic') #LABEdits
+    phantom = create_phantom() 
+    
+    
+    phantom = phantom.astype(np.float32)
+    phantom /= phantom.max()
+    phantom = all_images[0]
     # 2. Create ASTRA geometry
-    num_angles = 30  # Use 30 projection angles
+    
+    num_angles = 100  # Use 30 projection angles
     proj_geom, vol_geom = create_astra_geometry(phantom.shape, num_angles)
+
     
     # 3. Generate the sinogram
     print("Generating sinogram using ASTRA...")
     sinogram = generate_sinogram_astra(phantom, proj_geom, vol_geom)  # Pass vol_geom here
-    sinogram = add_noise(sinogram, noise_type='poisson', noise_level=1.0)
+    #sinogram = add_noise(sinogram, noise_type='poisson', noise_level=1.0)
     
+
     # Display the phantom and sinogram
+
     plt.figure(figsize=(12, 5))
     plt.subplot(121)
     plt.imshow(phantom, cmap='gray')
@@ -37,20 +45,22 @@ def main():
     # Save the sinogram
     save_image(sinogram, "results/sinogram.png")
 
-    # 4. Perform FBP reconstruction
-    print("Performing FBP reconstruction...")
-    fbp_reconstruction = reconstruct_fbp_astra(sinogram, proj_geom, vol_geom)
-    save_image(fbp_reconstruction, "results/fbp_reconstruction.png")
+    # 4. Perform SIRT reconstruction
+    print("Performing SIRT reconstruction...")
+
+    sirt_reconstruction = reconstruct_sirt_astra(sinogram, proj_geom, vol_geom, num_iterations=40)
+  
+    save_image(sirt_reconstruction, "results/sirt_reconstruction.png")
 
     # 5. Perform PDM-DART reconstruction
     print("Starting PDM-DART reconstruction with ASTRA...")
-    reconstructor = PDMDARTAstra(sinogram, phantom, phantom.shape, num_grey_levels=5) #LABEdits - sending phantom
-    reconstruction = reconstructor.reconstruct(num_iterations=20, sirt_iterations=10)
+    reconstructor = PDMDARTAstra(sinogram, phantom, phantom.shape, num_grey_levels=10) #LABEdits - sending phantom
+    reconstruction = reconstructor.reconstruct(num_iterations=20, sirt_iterations=20)
     save_image(reconstruction, "results/pdm_dart_reconstruction.png")
 
     # 6. Evaluate reconstruction quality
-    fbp_rmse = compute_rmse(phantom, fbp_reconstruction)
-    fbp_rnmp = compute_rnmp(phantom, fbp_reconstruction)
+    fbp_rmse = compute_rmse(phantom, sirt_reconstruction)
+    fbp_rnmp = compute_rnmp(phantom, sirt_reconstruction)
     pdm_rmse = compute_rmse(phantom, reconstruction)
     pdm_rnmp = compute_rnmp(phantom, reconstruction)
 
@@ -65,8 +75,8 @@ def main():
     plt.colorbar()
 
     plt.subplot(142)
-    plt.imshow(fbp_reconstruction, cmap='gray')
-    plt.title("FBP Reconstruction")
+    plt.imshow(sirt_reconstruction, cmap='gray')
+    plt.title("SIRT Reconstruction")
     plt.colorbar()
 
     plt.subplot(143)
